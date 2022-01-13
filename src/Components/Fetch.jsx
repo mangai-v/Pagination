d440701270f6141a60720629e365bfdd6d2c5cb7
mangai <mangailakshmi23@gmail.com>
2022-01-13 18:27:06 +0530

    lastest

--- src/Components/Fetch.jsx
+++ src/Components/Fetch.jsx
@@ -5,55 +5,80 @@ import Pagination from "./Pagination";
 const Fetch = () => {
   let [state, setState] = useState([]);
   let [filtered, setFiltered] = useState([]);
-  
-
-  useEffect(() => {
-    let fetchData = async () => {
-      let { data } = await Axios.get("todos");
-      try {
-        setState(data);
-        setFiltered(data.slice(0, 10));
-      } catch (error) {
-        console.log(error);
-      }
-      console.log(data);
-    };
-    fetchData();
-  }, []);
-  let handleClick = (page) => {
-    setFiltered(state.slice(page * 10 - 10, page * 10));
+  let [pageNum, setPageNum] = useState(0);
+  let perPage = 10;
+ let pageCount = Math.ceil(state.length / perPage);
+let visited = perPage * pageNum;
+let displayData = state.slice(visited, visited + perPage).map(val => (
+  <tr>
+    <td>{val.id}</td>
+    <td>{val.title}</td>
+    <td>{val.userId}</td>
+  </tr>
+));
+console.log(displayData);
+useEffect(() => {
+  let fetchData = async () => {
+    let { data } = await Axios.get("todos");
+    try {
+      setState(data);
+    } catch (error) {}
   };
+  fetchData();
+}, []);
+  let handleNext = () => {
+      setPageNum(pageNum+1)
+  }
+    let handlePre = () => {
+      setPageNum(pageNum -1);
+  };
+  let handleFirst = () => {
+      setPageNum(0)
+  }
+  let handleLast = () => {
+      setPageNum(pageCount-1)
+  }
+console.log(state);
+
+ 
 
   return (
-    <section className=" w-[100%] h-[100vh] my-16 justify-center" >
-      <article className=" w-[90%] mx-auto">
-        
-        <table className="border-collapse border border-gray-400 my-10 " >
-          <tr>
-            <th className="border border-gray-300">S.no</th>
-            <th className="align-middle">Id</th>
-            <th className="border border-gray-300">UserId</th>
-            <th className="border border-gray-300">Title</th>
-            <th className="border border-gray-300">Completed</th>
+    <div className="m-auto  text-3xl">
+      <table className=" w-[80%] m-auto border-8">
+        <thead className="ml-14">
+          <tr className="bg-slate-900 text-white text-lg border-8">
+            <td>ID</td>
+            <td>TITLE</td>
+            <td>USERID</td>
           </tr>
-        
-          {
-          filtered.map((x) => (
-            <tr>
-              <td className="border border-gray-300">{x.id}</td>
-              <td className="border border-gray-300">{x.id}</td>
-              <td className="border border-gray-300">{x.userId}</td>
-              <td className="border border-gray-300">{x.title}</td>
-              <td className="border border-gray-300">{x.completed}</td>
-            </tr>
-          ))
-          }
-          
-        </table>
-      </article>
+        </thead>
+        <tbody className="bg-slate-900 text-white">{displayData}</tbody>
+      </table>
+      <div className="flex justify-between m-auto w-[80%] ">
+        <button onClick={handleFirst} className="bg-slate-900 text-white">
+          First
+        </button>
 
-      <Pagination state={state} handleClick={handleClick} />
-    </section>
+        <button
+          onClick={handlePre}
+          className="bg-slate-900 text-white rounded-md"
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          className="bg-slate-900 text-white rounded-md"
+        >
+          Next
+        </button>
+        <button
+          onClick={handleLast}
+          className="bg-slate-900 text-white rounded-md"
+        >
+          Last
+        </button>
+      </div>
+    </div>
   );
 };
 
